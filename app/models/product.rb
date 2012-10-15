8bab94f5f6b11c5b08f6433fc35c45df9e0448f3
wfsiew <siewwingfei@hotmail.com>
2012-10-15 21:55:42 +0800

    commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,15 +7,15 @@ class Product < ActiveRecord::Base
   
   default_scope select([:category, :description, :imagemimetype, :name, :price, :productID])
   
-  validates_presence_of :name, :message => I18n.t('product.blank.name')
-  validates_presence_of :description, :message => I18n.t('product.blank.description')
-  validates_presence_of :price, :message => I18n.t('product.blank.price')
-  validates_numericality_of :price, :greater_than_or_equal_to => 0, :message => I18n.t('product.invalid.price')
-  validates_presence_of :category, :message => I18n.t('product.blank.category')
+  validates_presence_of :name, :message => 'product.blank.name'
+  validates_presence_of :description, :message => 'product.blank.description'
+  validates_presence_of :price, :message => 'product.blank.price'
+  validates_numericality_of :price, :greater_than_or_equal_to => 0, :message => 'product.invalid.price'
+  validates_presence_of :category, :message => 'product.blank.category'
   
   def show_error(key)
     if errors.has_key?(key)
-      content_tag(:span, errors.get(key).first, :class => 'form_error')
+      content_tag(:span, I18n.t(errors.get(key).first), :class => 'form_error')
     end
   end
 end
