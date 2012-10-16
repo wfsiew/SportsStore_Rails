f014e3db80184a79c8753eb7795d0bc197390a56
wfsiew <siewwingfei@hotmail.com>
2012-10-16 14:53:38 +0800

    commit

--- test/functional/order_test.rb
+++ test/functional/order_test.rb
@@ -1,12 +1,12 @@
-require 'test_helper'
+# require 'test_helper'
 
-class OrderTest < ActionMailer::TestCase
-  test "send_order" do
-    mail = Order.send_order
-    assert_equal "Send order", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+# class OrderTest < ActionMailer::TestCase
+  # test "send_order" do
+    # mail = Order.send_order
+    # assert_equal "Send order", mail.subject
+    # assert_equal ["to@example.org"], mail.to
+    # assert_equal ["from@example.com"], mail.from
+    # assert_match "Hi", mail.body.encoded
+  # end
 
-end
+# end
--- test/functional/product_controller_test.rb
+++ test/functional/product_controller_test.rb
@@ -4,4 +4,52 @@ class ProductControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @product = product(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_template 'productspage'
+    assert_not_nil assigns(:cart)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:products)
+    assert_not_nil assigns(:pager)
+    assert_not_nil assigns(:category)
+  end
+
+  test "should get index_paged" do
+    get :index_paged, {'page' => 1}
+    assert_response :success
+    assert_template 'productspage'
+    assert_not_nil assigns(:cart)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:products)
+    assert_not_nil assigns(:pager)
+    assert_not_nil assigns(:category)
+  end
+
+  test "should get category" do
+    get :category, {'category' => @product.category}
+    assert_response :success
+    assert_template 'productspage'
+    assert_not_nil assigns(:cart)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:products)
+    assert_not_nil assigns(:pager)
+    assert_not_nil assigns(:category)
+  end
+  
+  test "should get category_paged" do
+    get :category_paged, {'category' => @product.category, 'page' => 1}
+    assert_response :success
+    assert_template 'productspage'
+    assert_not_nil assigns(:cart)
+    assert_not_nil assigns(:categories)
+    assert_not_nil assigns(:products)
+    assert_not_nil assigns(:pager)
+    assert_not_nil assigns(:category)
+  end
 end
