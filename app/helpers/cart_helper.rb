2e9f1d023d9060cd7d4d9d0620f64fb89cfff26c
wfsiew <siewwingfei@hotmail.com>
2012-10-19 00:20:39 +0800

    commit

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,9 +1,10 @@
 module CartHelper
-  
+  # CartLine object used to store the productID and quantity
   class CartLine
     attr_accessor :productID, :quantity
   end
   
+  # Cart object used to store the CartLine objects in an Array
   class Cart
     attr_reader :cartlines
     
@@ -11,6 +12,7 @@ module CartHelper
       @cartlines = []
     end
     
+    # Add a product and the specified quantity to the CartLine Array.
     def add_item(product, quantity)
       i = @cartlines.index { |o| o.productID == product.productID }
       if i == nil
@@ -24,11 +26,13 @@ module CartHelper
       end
     end
     
+    # Remove the specified product from the CartLine Array.
     def remove_line(product)
       i = @cartlines.index { |o| o.productID == product.productID }
       @cartlines.delete_at(i) if i != nil
     end
     
+    # Get the total amount from the CartLine Array.
     def compute_total_value
       sum = 0.0
       @cartlines.each do |o|
@@ -38,16 +42,19 @@ module CartHelper
       sum
     end
     
+    # Get the total quantity from the CartLine Array.
     def total_quantity
       total = 0
       @cartlines.each { |o| total += o.quantity }
       total
     end
     
+    # Clear the CartLine Array.
     def clear
       @cartlines.clear
     end
     
+    # Get the Cart object from the session. It creates a new Cart object if the session doesn't has any Cart object.
     def self.cart(session)
       cart = session[:cart]
       if cart == nil
@@ -58,6 +65,7 @@ module CartHelper
     end
   end
   
+  # Shipping Details object for mail order
   class ShippingDetails
     include ActiveModel::Validations
     include ActionView::Helpers::TagHelper
@@ -71,7 +79,8 @@ module CartHelper
     validates_presence_of :country, :message => 'shinfo.blank.country'
     validates :email, :length => { :within => 5..50, :message => 'shinfo.blank.email' },
                       :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i, :message => 'shinfo.invalid.email' }
-
+                      
+    # Submit the order with the Cart details
     def submit_order?(cart)
       if valid?
         mail = Order.send_order(cart, self)
@@ -81,6 +90,7 @@ module CartHelper
       false
     end
     
+    # Show validation error message
     def show_error(key)
       if errors.has_key?(key)
         m = ''
--- test/functional/order_test.rb
+++ test/functional/order_test.rb
@@ -1,12 +1,36 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class OrderTest < ActionMailer::TestCase
-  # test "send_order" do
-    # mail = Order.send_order
-    # assert_equal "Send order", mail.subject
-    # assert_equal ["to@example.org"], mail.to
-    # assert_equal ["from@example.com"], mail.from
-    # assert_match "Hi", mail.body.encoded
-  # end
+class OrderTest < ActionMailer::TestCase
+  include CartHelper
+  
+  setup do
+    @product = product(:one)
+  end
+  
+  test "send_order" do
+    shippingdetails = ShippingDetails.new
+    shippingdetails.name = 'ben'
+    shippingdetails.email = 'ben@gmail.com'
+    shippingdetails.line1 = 'block 8'
+    shippingdetails.line2 = 'jalan maju'
+    shippingdetails.line3 = 'taman maju'
+    shippingdetails.city = 'K.L'
+    shippingdetails.state = 'W.P'
+    shippingdetails.zip = 56000
+    shippingdetails.country = 'malaysia'
+    
+    cart = Cart.new
+    cart.add_item(@product, 9)
+    
+    mail = Order.send_order(cart, shippingdetails)
+    mail.deliver
+    
+    assert !ActionMailer::Base.deliveries.empty?
+    assert_equal [shippingdetails.email], mail.to
+    assert_equal "New order submitted!", mail.subject
+    assert_equal ["admin@localhost"], mail.from
+    assert_match /<h3>A new order has been submitted<\/h3>/, mail.body.encoded
+    assert_match /#{shippingdetails.name}/, mail.body.encoded
+  end
 
-# end
+end
